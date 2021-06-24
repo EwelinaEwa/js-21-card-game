1e7c570f8d2ec46553729d5e033b1dee60dd0028
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-24 12:24:19 +0200

    Add first two player cards to document + add icons for card colors

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-const colors = [" hearts", " diamonds", " spades", " clubs"]
+const colors = [" ♥", " ♦", " ♠", " ♣"]
 
 function drawRandomCard() {
     let randomCard = cards[Math.floor(Math.random() * cards.length)];
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     let drawnCard = randomCard+randomColor;
-    return drawnCard
+    return drawnCard;
 }
 
 let drawnCard1 = drawRandomCard()
 let drawnCard2 = drawRandomCard()
+
+let userCards = [drawnCard1, drawnCard2]
+
+document.getElementById("firstDraw").innerHTML = userCards;
 
 console.log(drawnCard1)
 console.log(drawnCard2)
